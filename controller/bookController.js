0be17b032132b43aa98a3598562ba805a76dbc00
ovovvvvv <ovovvvvv@naver.com>
2024-01-04 19:27:52 +0900

    #1 카테고리&&신간 여부로 전체 도서 조회, 페이징

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,22 +2,32 @@ const { STATUS_CODES } = require('http');
 const conn = require('../mariadb') // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
+// (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-    let {category_id, newBook} = req.query;
+    let {category_id, newBook, limit, currentPage} = req.query;
+
+    // limit : page당 도서 수
+    // currentPage : 현재 몇 페이지인지 ex. 1, 2, 3 ...
+    // offset : 0, 3, 6, .... 
+    // limit * (currentPage - 1)
+    let offset = limit * (currentPage - 1);
 
     let sql = "SELECT * FROM books";
     let values = [];
   
     if(category_id && newBook){
         sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
-        values = [category_id, newBook];
+        values.push(category_id);
     } else if (category_id){
         sql += " WHERE category_id = ?";
-        values = category_id;
+        values.push(category_id);
     } else if (newBook){
         sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
-        values = newBook;
     }
+
+    sql += " LIMIT ? OFFSET ?";
+    values.push(parseInt(limit), offset);
+
     conn.query(sql, values,
         (err, results) => {
             if(err){
